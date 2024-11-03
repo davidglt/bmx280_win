8583135ab356f5e7066cac952c5923ddf31225c6
David Gonzalez <davidglt@hotmail.com>
2024-11-03 19:24:42 +0100

    Add Pa unit in the pressure

--- sensor.py
+++ sensor.py
@@ -3,5 +3,5 @@ import bmx280
 sensor = bmx280.BMX280(0, 0x76)
 
 print(f"Temperature: {sensor.temperature} °C")
-print(f"Pressure: {sensor.pressure}")
+print(f"Pressure: {sensor.pressure} Pa")
 print(f"Humidity: {sensor.humidity} %")
